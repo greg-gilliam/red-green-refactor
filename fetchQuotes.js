459f640860913aeeb36ba51dcecb66a9d54ea65c
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-09-13 17:54:54 -0700

    still working on fetchQuotes

---
+++ fetchQuotes.js
@@ -0,0 +1,11 @@
+// import fetch from 'node-fetch';
+
+// const fetchQuotes = async() => {
+//     const url = 'http://futuramaapi.herokuapp.com/quotes';
+//     const res = await fetch(url);
+//     const body = await res.json();
+
+//     return body.map(())
+// };
+
+// module.exports = fetchQuotes;
